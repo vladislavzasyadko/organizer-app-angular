ec1c4e296bf06be31e74b9b63c4663e3d142fad8
vlad <gilbert.cook111@gmail.com>
2020-03-13 17:57:54 +0300

    project structure initiated selector component addded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
+import { MomentPipe } from './shared/moment.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe,
   ],
   imports: [
     BrowserModule
