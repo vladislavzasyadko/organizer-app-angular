fa46115dda92fbf0497447706bf3c572eee88107
vlad <gilbert.cook111@gmail.com>
2020-03-14 17:15:08 +0300

    added removing tasks functionality

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -3,6 +3,7 @@ import { DateService } from './../shared/date.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {Task} from '../shared/tasks.service'
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organizer',
@@ -12,10 +13,17 @@ import {Task} from '../shared/tasks.service'
 export class OrganizerComponent implements OnInit {
 
   form: FormGroup
+  tasks: Task[] = []
 
   constructor(private dateService: DateService, private tasksService: TasksService) { }
 
   ngOnInit() {
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
     this.form =new FormGroup({
       title: new FormControl('', Validators.required)
     })
@@ -29,9 +37,16 @@ export class OrganizerComponent implements OnInit {
     }
     console.log('task before service ', task)
     this.tasksService.create(task).subscribe(task => {
-      console.log('new task', task)
+      this.tasks.push(task)
       this.form.reset()
     }, err => console.error(err))
+  }
+
+  remove(task: Task){
+    this.tasksService.remove(task).subscribe(
+      () => {
+        this.tasks = this.tasks.filter(t => t.id !== task.id)
+      }, err => console.error(err))
   }
 
 }
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 export interface Task {
     id?: string
@@ -21,8 +22,22 @@ export class TasksService {
 
     }
 
+    load(date: moment.Moment): Observable<Task[]>{
+        return this.http.get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+        .pipe(map(tasks =>{
+            if(!tasks){
+                return []
+            }
+            return Object.keys(tasks).map(key =>({...tasks[key], id: key}))
+        }))
+    }
+
     create(task: Task): Observable<Task>{
         return this.http.post<CreateResponse>(`${TasksService.url}/${task.date}.json`, task)
         .pipe(map( response => ({...task, id: response.name})))
+    }
+
+    remove(task: Task): Observable<void> {
+        return this.http.delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`)
     }
 }
